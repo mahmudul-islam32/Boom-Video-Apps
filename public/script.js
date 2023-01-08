7969b53fdaf1d6bf73fa0d2091b18800abefb7a4
Mahmudul Islam <mahmud@iMac-von-Mahmudul.local>
2023-01-08 23:13:23 +0100

    Complete Video stop and mute

--- public/script.js
+++ public/script.js
@@ -52,3 +52,71 @@ const addVideoStream = (video,stream) => {
    })
    videoGrid.append(video);
 }
+
+const scrollToBottom = () => {
+    let d = $('.main_chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () => {
+    const enabled = videoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        videoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        videoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const playStop = () => {
+    console.log('object')
+    let enabled = videoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        videoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      videoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+
+const setMuteButton = () => {
+    const html = ` <i class="fas fa-microphone"></i> <span>Mute</span> `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = ` <i class="unmute fas fa-microphone-slash"></i> <span>Unmute</span> `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+
+
+const setStopVideo = () => {
+    const html = `<i class="fas fa-video"></i>
+    <span>Stop Video</span>`
+    document.querySelector('.main_video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `<i class="stop fas fa-video"></i>
+    <span>Play Video</span>`
+    document.querySelector('.main_video_button').innerHTML = html;
+}
+
+let text = $('input')
+console.log(text)
+
+$('html').keydown((e) => {
+   if(e.which == 13 && text.val().length !== 0){
+    console.log(text.val())
+    socket.emit('message', text.val());
+    text.val('')
+   }
+})
+
+socket.on('createMessage', message => {
+    $('ul').append(`<li class="message"><b>user</b><br/>${message}</li>`)
+    scrollToBottom()
+})
